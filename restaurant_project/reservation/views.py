845148bc6543707337dbd811c06ba59f5ad0f6de
ejiro-design <ajirogene55@gmail.com>
2022-10-29 00:36:54 +0000

    add app name to installed apps

--- restaurant_project/reservation/views.py
+++ restaurant_project/reservation/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('reservation/index.html')
+    context = {
+        'latest_question_list': 3,
+    }
+    return HttpResponse(template.render(context, request))
