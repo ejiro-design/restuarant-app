e20c0a18af2cea6923cd3eb3f8a240fcb954d171
ejiro-design <ajirogene55@gmail.com>
2022-10-30 22:21:00 +0000

    add menu and bookings models.py

--- restaurant_project/reservation/models.py
+++ restaurant_project/reservation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+
+
+class Bookings(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    seats = models.IntegerField(default=0)
+    date = models.DateField()
+    time = models.TimeField()
--- restaurant_project/reservation/views.py
+++ restaurant_project/reservation/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Menu
+from .models import Bookings
 
 def index(request):
     template = loader.get_template('reservation/index.html')
