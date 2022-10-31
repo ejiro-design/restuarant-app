449ea69301ca07969855425e124d3a867a23d1b7
ejiro-design <ajirogene55@gmail.com>
2022-10-31 16:25:49 +0000

    add a new repository

---
+++ reservation/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Bookings(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    seats = models.IntegerField(default=0)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.name + ' (' + self.email + ')'
---
+++ reservation/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Menu
+from .models import Bookings
+
+
+def index(request):
+    context = {'menu': "", 'error': "", 'success': ""}
+    if (request.POST):
+        try:
+            existing = Bookings.objects.get(name=request.POST['name'], email=request.POST['email'], date=request.POST['date'], time=request.POST['time'])
+            context['error'] = 'Double booking detected'
+        except:
+            menuOption = Menu.objects.get(id=request.POST['menu'])
+            query = Bookings(name=request.POST['name'], email=request.POST['email'], menu=menuOption, seats=request.POST['seats'], date=request.POST['date'], time=request.POST['time'])
+            query.save()
+            context['success'] = "Booking sucessfull"
+    menuItems = Menu.objects.order_by('name')
+    template = loader.get_template('reservation/index.html')
+    context['menu'] = menuItems
+    return HttpResponse(template.render(context, request))
