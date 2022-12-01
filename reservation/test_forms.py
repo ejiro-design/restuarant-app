34bbe346e9ac1d414abb16415cb632d10928063e
ejiro-design <ajirogene55@gmail.com>
2022-12-01 21:42:03 +0000

    add test to form

--- reservation/test_forms.py
+++ reservation/test_forms.py
@@ -11,4 +11,25 @@ class TestItemform(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.error.keys())
         self.assertEqual(forms.error['name'][0], 'This field is required')
-        
+
+    def test_email_field_required(self):
+        form = Itemform({'email', " "})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.error.keys())
+        self.assertEqual(form.error, [email], 'This field is required')
+
+    def test_date_field_required(self):
+        form = Itemform({'date', " "})
+        self.assertFalse(form.is_valid())
+        self.assertIn('date', form.error.keys())
+        self.assertEqual(form.error, [date], 'This field is required')
+
+    def test_time_field_required(self):
+        form = Itemform({'time', " "})
+        self.assertFalse(form.is_valid())
+        self.assertIn('time', form.error.keys())
+        self.assertEqual(form.error, [time], 'This field is required')
+
+    def test_seats_field_not_required(self):
+        form = Itemform({'seats', " "})
+        self.assertTrue(form.is_valid())
